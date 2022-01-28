c3161666a22d1191f4ce4072a7f38ac1dc00bbbf
Andrew Po <ensimgen@gmail.com>
2022-01-28 07:48:43 +0300

    Extracted Data Source Methods.

---
+++ Today/ReminderListDataSource.swift
@@ -0,0 +1,38 @@
+//
+//  ReminderListDataSource.swift
+//  Today
+//
+//  Created by Dreik on 1/28/22.
+//
+
+import UIKit
+
+class ReminderListDataSource: NSObject {
+    
+}
+
+extension ReminderListDataSource: UITableViewDataSource {
+    static let reminderListCellIdentifier = "ReminderListCell"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Reminder.testData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let  cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
+            fatalError("Unable to dequeue ReminderCell")
+        }
+        let reminder = Reminder.testData[indexPath.row]
+        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        cell.titleLabel.text = reminder.title
+        cell.dateLabel.text = reminder.dueDate.description
+        cell.doneButton.setBackgroundImage(image, for: .normal)   // for the old behavior, need to change the Style pop-up menu (in the Attributes inspector) from Plain to Default. (in iOS 15 the button works different)
+        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
+            Reminder.testData[indexPath.row].isComplete.toggle()
+            tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        
+        return cell
+    }
+
+}
--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -21,27 +21,3 @@ class ReminderListViewController: UITableViewController {
     }
 }
 
-extension ReminderListViewController {
-    static let reminderListCellIdentifier = "ReminderListCell"
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let  cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
-            fatalError("Unable to dequeue ReminderCell")
-        }
-        let reminder = Reminder.testData[indexPath.row]
-        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
-        cell.titleLabel.text = reminder.title
-        cell.dateLabel.text = reminder.dueDate.description
-        cell.doneButton.setBackgroundImage(image, for: .normal)   // for the old behavior, need to change the Style pop-up menu (in the Attributes inspector) from Plain to Default. (in iOS 15 the button works different)
-        cell.doneButtonAction = {
-            Reminder.testData[indexPath.row].isComplete.toggle()
-            tableView.reloadRows(at: [indexPath], with: .none)
-        }
-        
-        return cell
-    }
-}
