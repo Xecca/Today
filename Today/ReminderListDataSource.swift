18893de561d55ca21170a1b2ece8dbe66ee373c3
Andrew Po <ensimgen@gmail.com>
2022-01-29 13:00:58 +0300

    Changed date format of in list cells and detail cells.

--- Today/ReminderListDataSource.swift
+++ Today/ReminderListDataSource.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ReminderListDataSource: NSObject {
-    
+    private lazy var dateFormatter = RelativeDateTimeFormatter()
 }
 
 extension ReminderListDataSource: UITableViewDataSource {
@@ -23,11 +23,12 @@ extension ReminderListDataSource: UITableViewDataSource {
             fatalError("Unable to dequeue ReminderCell")
         }
         let reminder = Reminder.testData[indexPath.row]
-        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
-        cell.titleLabel.text = reminder.title
-        cell.dateLabel.text = reminder.dueDate.description
-        cell.doneButton.setBackgroundImage(image, for: .normal)   // for the old behavior, need to change the Style pop-up menu (in the Attributes inspector) from Plain to Default. (in iOS 15 the button works different)
-        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
+//        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        let dateText = dateFormatter.localizedString(for: reminder.dueDate, relativeTo: Date())
+//        cell.titleLabel.text = reminder.title
+//        cell.dateLabel.text = reminder.dueDate.description
+//        cell.doneButton.setBackgroundImage(image, for: .normal)   // for the old behavior, need to change the Style pop-up menu (in the Attributes inspector) from Plain to Default. (in iOS 15 the button works different)
+        cell.configure(title: reminder.title, dateText: dateText, isDone: reminder.isComplete) {
             Reminder.testData[indexPath.row].isComplete.toggle()
             tableView.reloadRows(at: [indexPath], with: .none)
         }
