66ceac6ab995d22975bfd6f40ffdc1a8732843a5
Andrew Po <ensimgen@gmail.com>
2022-02-01 14:54:47 +0300

    Found some bugs with replacement of cells with date and notes by title strings.

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 
 class ReminderListViewController: UITableViewController {
-    private var reminderListDataSource: ReminderListDataSource?
+    
     static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+    static let mainStoryboardName = "Main"
+    static let detailViewControllerIdentifier = "ReminderDetailViewController"
+    
+    private var reminderListDataSource: ReminderListDataSource?
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == Self.showDetailSegueIdentifier,
@@ -31,6 +35,30 @@ class ReminderListViewController: UITableViewController {
         super.viewDidLoad()
         reminderListDataSource = ReminderListDataSource()
         tableView.dataSource = reminderListDataSource
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let navigationController = navigationController,
+           navigationController.isToolbarHidden {
+            navigationController.setToolbarHidden(false, animated: animated)
+        }
+    }
+    
+    @IBAction func addButtonTriggered(_ sender: UIBarButtonItem) {
+        addReminder()
+    }
+    
+    private func addReminder() {
+        let storyboard = UIStoryboard(name: Self.mainStoryboardName, bundle: nil)
+        let detailViewController: ReminderDetailViewController = storyboard.instantiateViewController(identifier: Self.detailViewControllerIdentifier)
+        let reminder = Reminder(title: "New Reminder", dueDate: Date())
+        detailViewController.configure(with: reminder, isNew: true, addAction: { reminder in
+            self.reminderListDataSource?.add(reminder)
+            self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+        })
+        let navigationController = UINavigationController(rootViewController: detailViewController)
+        present(navigationController, animated: true, completion: nil)
     }
 }
 
